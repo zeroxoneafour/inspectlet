35b9cecec42715cb448b91ea6e1ce976d55389c0
Vaughan Milliman <vaughanm1@protonmail.com>
2022-02-26 17:43:34 -0600

    finally remerged inspectlet and consolet

--- inspectlet.js
+++ inspectlet.js
@@ -1,5 +1,13 @@
 // inspectlet.js
 
+// code stolen from StackOverflow - keeps a console log
+console.stdlog = console.log.bind(console);
+inspectlet_consoleLog = [];
+console.log = function(){
+    inspectlet_consoleLog.push(Array.from(arguments));
+    console.stdlog.apply(console, arguments);
+};
+
 // html inspect page
 var inspectlet_HTML = "<!DOCTYPE html>" +
 "<html>" +
@@ -8,11 +16,22 @@ var inspectlet_HTML = "<!DOCTYPE html>" +
 "</head>" +
 "<body>" +
 "<h3>Inspectlet Inspector</h3>" +
+"<div>" +
+"<input type='button' value='Inspector' id='switchInspector'></input>" +
+"<input type='button' value='Console' id='switchConsole'></input>" +
+"<div class='tool' id='inspector' style='display: box'>" +
 "<div style='display: flex; flex-direction: row; margin: 0 auto; width: 100%;'>" +
 "<input type='button' value='Read Document' id='read'></input>" +
 "<input type='button' value='Write Document' id='write'></input>" +
 "</div>" +
-"<textarea id='textArea' style='display: box; width: 100%; height: 100%; font-family: monospace;'></textarea>" +
+"<textarea id='textArea' style='width: 100%; height: 100%; font-family: monospace;'></textarea>" +
+"</div>" +
+"<div class='tool' id='console' style='display: hidden'>" +
+"<div style='display: flex; flex-flow: column; height: 100%; width: 100%;'>" +
+"<p id='consoleOutput' style='font-family: monospace; white-space: pre;'></p>" +
+"</div>" +
+"<input type='text' id='consoleInput' style='font-family: monospace; width: 100%;'></input>" +
+"</div>" +
 "</body>" +
 "</html>";
 
@@ -31,6 +50,26 @@ function inspectlet_writeDocument() {
 	document.close();
 };
 
+// read the console
+function inspectlet_readConsole() {
+	inspectlet_window.getElementById("consoleOutput").innerHTML = inspectlet_consoleLog.join('\n');
+};
+
+// eval instructions
+function inspectlet_eval() {
+	console.log("> " + inspectlet_window.getElementById("consoleInput").value);
+	console.log(Function("return " + (inspectlet_window.getElementById("consoleInput").value)) ());
+	inspectlet_window.getElementById("consoleInput").value = "";
+	inspectlet_readConsole();
+};
+
+function inspectlet_switch(tab) {
+	for ( const inspectlet_tab in inspectlet_window.getElementsByClassName("tool") ) {
+		inspectlet_tab.style.display = "none";
+	};
+	inspectlet_window.getElementById(tab).style.display = "block";
+};
+
 // create the inspector
 function inspectlet_createInspector() {
 	inspectlet_window = window.open("about:blank", "inspectletWindow", "popup").document;
@@ -38,9 +77,21 @@ function inspectlet_createInspector() {
 	inspectlet_window.write(inspectlet_HTML);
 	inspectlet_window.close();
 
-	// Connect functions
+	// stuff for the tab switching
+	inspectlet_window.getElementById("switchInspector").addEventListener("click", inspectlet_switch("inspector"));
+	inspectlet_window.getElementById("switchConsole").addEventListener("click", inspectlet_switch("console");
+
+	// Connect other functions
 	inspectlet_window.getElementById("read").addEventListener("click", inspectlet_readDocument);
 	inspectlet_window.getElementById("write").addEventListener("click", inspectlet_writeDocument);
+
+	// create listener for input
+	inspectlet_window.getElementById("consoleInput").addEventListener("keyup", function(event) {
+		if (event.keyCode === 13) {
+			inspectlet_eval();
+		};
+	});
+
 };
 
 inspectlet_createInspector();

32d4dfccc921f3c23bdacf962c421c4e0a88540a
Vaughan Milliman <vaughanm1@protonmail.com>
2022-02-25 19:50:18 -0600

    fixed the last? error of not putting .document

--- payload.js
+++ payload.js
@@ -43,7 +43,7 @@ function inspectlet_test() {
 }
 
 function inspectlet_readDocument() {
-	inspectlet_window.getElementById("inspectorTextArea").value = inspectorXMLSerializer().serializeToString(document);
+	inspectlet_window.document.getElementById("inspectorTextArea").value = inspectorXMLSerializer().serializeToString(document);
 }
 
 function inspectlet_writeDocument() {
